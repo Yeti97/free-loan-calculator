d94e020c56bf435086c63aa7416a2e8496d0b51a
Macaulay White <macaulaywhite1997@gmail.com>
2020-08-17 11:57:34 +0100

    CompoundChart
    Label improvements

--- src/CompoundChart.js
+++ src/CompoundChart.js
@@ -21,9 +21,9 @@ export default function CompoundChart(props) {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="year" />
-        <YAxis dataKey="amount" domain={[initial, totalCost]} />
-        <Tooltip />
+        <XAxis dataKey="year" label="Year"/>
+        <YAxis dataKey="amount" label="Total" domain={[initial, totalCost]} />
+        <Tooltip label="Year"/>
         <Legend />
         <Line type="monotone" dataKey="amount" stroke="#8884d8" activeDot={{ r: 8 }} />
       </LineChart>
